92ade6422b3d32954e5577143b8d05e70f70207f
Franco Arriola <francosebastianarriola@gmail.com>
2022-11-23 17:08:52 -0300

    cambiando metas a app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import "bootstrap/scss/bootstrap.scss";
 import "../styles/globals.scss";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Omnifit Gym Gimnasio en Concordia, Entre Rios</title>
+        <meta
+          name="description"
+          content="Gimnasio Premium de +1000m2 con sala de Musculacion, Box de Crossfit y Box Funcional Hiit en Concordia"
+        />
+        <link rel="icon" href="/logofavicon.png" key="desc" />
+      </Head>
       <Component {...pageProps} />
       <Analytics />
     </>
--- pages/index.js
+++ pages/index.js
@@ -9,17 +9,6 @@ import Head from "next/head";
 const Welcome = () => {
   return (
     <>
-      <Head>
-        <title>
-          Omnifit Gym │ Gimnasio en Concordia │ Sala de Musculacion │ Crossfit │
-          Hiit
-        </title>
-        <meta
-          name="description"
-          content="Gimnasio Premium de +1000m2 con sala de Musculacion, Box de Crossfit y Box Funcional Hiit en Concordia"
-        />
-        <link rel="icon" href="/logofavicon.png" key="desc" />
-      </Head>
       <Home></Home>
       <About></About>
       <Open></Open>
